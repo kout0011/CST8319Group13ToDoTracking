b37eeb50fc4f287f1d8e0ed076f37916b116fb60
Derek <kout0011@algonquinlive.com>
2024-08-04 19:54:15 -0400

    Initial commit

---
+++ app/src/main/java/cst8319/group13/todotracking/TaskEditPage.java
@@ -0,0 +1,14 @@
+package cst8319.group13.todotracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TaskEditPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_edit_page);
+    }
+}
---
+++ app/src/main/java/cst8319/group13/todotracking/UserHomePage.java
@@ -0,0 +1,14 @@
+package cst8319.group13.todotracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_home_page);
+    }
+}
